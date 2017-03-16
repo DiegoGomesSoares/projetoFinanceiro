b94b8577d6ce5b4b44a68665888262981bd9eeef
diego_soares <diegogomessoares@gmail.com>
2017-03-16 20:52:09 -0300

    Configurando o entity junto do banco de dados (fazendo os migrations iniciais)

---
+++ Financas/Financas/DAO/FinancasContexto.cs
@@ -0,0 +1,22 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContexto : DbContext
+    {
+        public DbSet<Usuario> Usarios { get; set; }
+
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+            
+        }
+    }
+}
