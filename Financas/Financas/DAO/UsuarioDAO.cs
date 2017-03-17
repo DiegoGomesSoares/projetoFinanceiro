f79d6859d7f640dd9f7e76fc9399191431c14b60
diego_soares <diegogomessoares@gmail.com>
2017-03-16 22:09:43 -0300

    adicionado o package de injeção de dependencias e PM> install-package ninject.mvc3 -version 3.2.1  para configurar que o usuario dao por requisição e tambpem feito o cadastro de usuario

---
+++ Financas/Financas/DAO/UsuarioDAO.cs
@@ -0,0 +1,32 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class UsuarioDAO
+    {
+        private FinancasContexto context;
+
+        public UsuarioDAO(FinancasContexto context)
+        {
+            //foi adicionado a package de injecao de dependencia PM> install-package ninject.mvc3 -version 3.2.1,
+            //para usar um mesmo contexto por requisição
+            //estão dentro da pasta app-start
+            this.context = context;
+        }
+
+        public void Adiciona(Usuario usuario)
+        {
+            context.Usarios.Add(usuario);
+            context.SaveChanges();
+        }
+
+        public IList<Usuario> Lista()
+        {
+            return context.Usarios.ToList();
+        }
+    }
+}
